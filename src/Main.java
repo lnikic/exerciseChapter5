51018a82e98979ca5ebb1e6e526852673054f112
nikic <nikic.luka2001@gmail.com>
2025-11-10 20:49:36 +0100

    finish exercises from chapter 5

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,34 @@
 public class Main {
     public static void main(String[] args) {
 
+        Output output = new Output();
+        output.go();
 
+        for (int i = 0; i < 4; i++) {
+
+            for (int j = 4; j > 2; j--) {
+                System.out.println(i + " " + j);
+            }
+            if (i == 1) {
+                i++;
+
+            }
+
+
+        }
+        int x = 0;
+        int y = 30;
+        for (int outer = 0; outer < 3; outer++) {
+            for (int inner = 4; inner > 1; inner--) {
+                x = x + 0;
+                y = y - 2;
+                if (x == 6) {
+                    break;
+                }
+                x = x + 3;
+            }
+            y = y - 2;
+        }
+        System.out.println(x + " " + y);
     }
 }
---
+++ src/Output.java
@@ -0,0 +1,15 @@
+public class Output {
+    void go() {
+        int value = 7;
+        for (int i = 1; i < 8; i++) {
+            value++;
+            if (i > 4) {
+                System.out.print(++value + " ");
+            }
+            if (value > 14) {
+                System.out.println(" i = " + i);
+                break;
+            }
+        }
+    }
+}
